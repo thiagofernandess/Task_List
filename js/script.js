acad14c5ebd96089c572aa8613776e39cf536963
Thiago Fernandes <thatawesomeid@gmail.com>
2022-08-18 17:05:08 -0300

    Adicionado funcionalidade de check e un-check em tasks clicadas

--- js/script.js
+++ js/script.js
@@ -13,11 +13,11 @@ function addTarefa() {
         ++contador;
         
         let novoItem = `<div id="${contador}" class="item">
-        <div class="item-icone">
-            <i class="mdi mdi-circle-outline"></i>
+        <div onclick="marcarTarefa(${contador})" class="item-icone">
+            <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
 
         </div>
-        <div class="item-nome">
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
             ${valorInput}
 
         </div>
@@ -41,6 +41,26 @@ function deletar(id){
     var tarefa = document.getElementById(id);
     //FUNÇÃO .remove DELETA O VALOR RECUPERADO ACIMA
     tarefa.remove();
+}
+
+function marcarTarefa(id){
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+    console.log(classe);
+
+    if (classe == "item") {
+        item.classList.add('clicado');
+
+        var icone = document.getElementById('icone_' + id)
+        icone.classList.remove('mdi-circle-outline');
+        icone.classList.add('mdi-check-circle');
+    } else{
+        item.classList.remove('clicado');
+
+        var icone = document.getElementById('icone_' + id)
+        icone.classList.remove('mdi-check-circle');
+        icone.classList.add('mdi-circle-outline');
+    }
 }
 
 //PARA ADICIONAR INPUT TECLANDO 'ENTER'(13) AO INVÉS DE CLICAR COM O MOUSE
